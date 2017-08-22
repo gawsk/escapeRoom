6647e2526b3b0ada6ec626985073aad953acab6d
Morgan Larkin <morganlarkin@aol.com>
2017-08-21 21:12:32 -0700

    Finally done

--- Escape Room Website/roomScript.js
+++ Escape Room Website/roomScript.js
@@ -8,8 +8,19 @@ function clearItAll() {
 
 function submitItAll() {
 	if (document.getElementById('d').value == "1234") {
-		document.getElementById('d').value = "YESYESYESYES";
-	} else {
+		show('part2', 'part1');
+		document.getElementById('part3').style.display='none';
 		clearItAll();
+	} else if (document.getElementById('d').value == "HELPID") {
+		//show('part3', 'part2');
+		document.getElementById('d').value='YOU DID IT CONGRATS!!!!!!!!!';
+		document.getElementById('part3').style.display='block';
 	}
+	
+}
+
+function show(shown, hidden) {
+  document.getElementById(shown).style.display='block';
+  document.getElementById(hidden).style.display='none';
+  return false;
 }
